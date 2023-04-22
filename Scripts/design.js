70d41bf8838c6f1fc2b46fcb5e966fd7b243ed79
LjayMoh <91534735+LjayMoh@users.noreply.github.com>
2023-04-22 12:58:24 +0200

    Add files via upload

---
+++ Scripts/design.js
@@ -0,0 +1,112 @@
+let calcScrollValue = () => {
+    let scrollProgress = document.getElementById("top-butt");
+    let top = document.getElementById("top");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight = 
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100) / calcHeight);
+    if (pos > 100) {
+      scrollProgress.style.display = "grid";
+    } else {
+      scrollProgress.style.display = "none";
+    }
+    scrollProgress.addEventListener("click", () => {
+      document.documentElement.scrollTop = 0;
+    });
+};
+  
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
+  
+const buttons = document.querySelectorAll("[data-carousel-button]")
+  
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+      const offset = button.dataset.carouselButton === "next" ? 1 : -1
+      const slides = button.closest("[data-carousel]").querySelector('[data-slides]')
+      const activeSlide = slides.querySelector("[data-active]")
+      let newIndex = [...slides.children].indexOf(activeSlide) + offset
+      if (newIndex < 0) newIndex = slides.children.length - 1
+      if (newIndex >= slides.children.length) newIndex = 0
+  
+      slides.children[newIndex].dataset.active = true
+      delete activeSlide.dataset.active
+    })
+})
+  
+$(document).ready(function() {
+    $('.post-wrapper').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 9000, 
+      nextArrow:$('.next'),
+      prevArrow:$('.prev'), 
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
+  
+$('.site-wrapper').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 8000, 
+    nextArrow:$('.nxt'),
+    prevArrow:$('.prv'), 
+    responsive: [
+        { 
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+       ]
+    });
+});
